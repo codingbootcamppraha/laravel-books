1a36f97cacf4f4cdf3370d9bf25e958740613c16
Jan Polak <jan.polak@onmyway.cz>
2021-07-09 14:34:39 +0200

    sample feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,33 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_homepage_response_is_200()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_sign_in_response_is_200()
+    {
+        $response = $this->get('/sign-in');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_sign_up_response_is_200()
+    {
+        $response = $this->get('/sign-up');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_eshop_page_works()
+    {
+        $response = $this->get('/eshop');
+
+        $response->assertStatus(200);
+
+        $response->assertSee('<h1>Bookshop project</h1>', false);
     }
 }
