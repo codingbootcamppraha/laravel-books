81dd5f6c7bd58bb360c175dbea09460992d92915
Mmarkev <monika@data4you.cz>
2024-06-17 15:24:29 +0200

    added vite

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,12 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/test.scss')
 </head>
 <body>
     @include('common.navigation', [
         'current_page' => $current_page
     ])
+
+    <div id="partners"></div>
     @yield('content')
     @yield('content-test')
+
+
+    @viteReactRefresh
+    @vite('resources/js/partners.jsx')
 </body>
 </html>
