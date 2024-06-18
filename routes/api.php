1a252fd77c8379a8dc3e8b06a01b627e68554c9c
Mmarkev <monika@data4you.cz>
2024-06-18 16:10:47 +0200

    Route class imported in api routes file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', [App\Http\Controllers\Api\TestController::class, 'index']);
 Route::get('/books/latest', [App\Http\Controllers\Api\BookController::class, 'latest']);
 Route::get('/books/search', [App\Http\Controllers\Api\BookController::class, 'search']);
